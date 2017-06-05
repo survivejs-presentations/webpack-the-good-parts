77ae7366d2851614d6e0ee7022de60dd4c4841a2
Juho Vepsalainen <bebraw@gmail.com>
2017-06-05 15:17:00 +0200

    Simplify

--- presentation/index.js
+++ presentation/index.js
@@ -99,7 +99,7 @@ export default class Presentation extends React.Component {
           <CodePane lang="javascript">
         {`{
   entry: {
-    app: path.join(__dirname, 'app', 'entry.js'),
+    app: path.join(__dirname, 'app'),
   },
 }`}
           </CodePane>
@@ -188,7 +188,7 @@ export default class Presentation extends React.Component {
           <CodePane lang="javascript">
         {`module.exports = {
   entry: {
-    app: path.join(__dirname, 'app', 'entry.js'),
+    app: path.join(__dirname, 'app'),
   },
   output: {
     path: path.join(__dirname, 'build'),
